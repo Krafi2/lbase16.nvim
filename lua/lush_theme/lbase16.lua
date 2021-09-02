8fd424d63bcae9cd1f7d10b05303587095da4f1f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-02 10:56:29 +0200

    Improved colorscheme organization

--- lua/lush_theme/lbase16.lua
+++ lua/lush_theme/lbase16.lua
@@ -1,23 +1,16 @@
 local lush = require "lush"
 
 -- Get all lua files located under the lush colorscheme
-local files = vim.api.nvim_get_runtime_file("lua/lush_theme/lbase16/*", true)
+local files = vim.api.nvim_get_runtime_file(
+    "lua/lush_theme/lbase16/**/*.lua",
+    true
+)
 
 local theme = lush.parse(function()
     return {}
 end)
 
-local loaded = {
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/_editor.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/_syntax.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/barbar.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/indent-blankline.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/lsp.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/nvim-tree.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/symbols-outline.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/treesitter.lua"] = true,
-    -- ["/home/goose/.local/share/nvim/site/pack/packer/start/lbase16.nvim/lua/lush_theme/lbase16/trouble.lua"] = true,
-}
+local loaded = {}
 for _, file in pairs(files) do
     -- There is a bug where `nvim_get_runtime_file` returns duplicates:
     -- https://github.com/neovim/neovim/issues/14294
